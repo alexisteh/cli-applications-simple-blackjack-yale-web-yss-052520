26271d42ecf60c26a4d39f7e75bfb42ff79a9bbb
Alexis Teh <wenhui.teh23@gmail.com>
2020-05-22 09:29:44 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -35,7 +35,7 @@ def hit?(cardtotal)
   prompt_user 
   input = get_user_input
   if input == 's' 
-    return 
+    return cardtotal 
   elsif input == 'h' 
     new = deal_card 
     cardtotal += new 
